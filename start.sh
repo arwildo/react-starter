ebd14cdf7244f13ebd3110951cb738559667d9d7
arwildo <arwildo@gmail.com>
2020-12-24 21:17:31 +0900

    change repo to react-starter

---
+++ start.sh
@@ -0,0 +1,9 @@
+npm init
+
+# React, Webpack, Babel, CSS Loader
+sudo npm i react react-dom -S
+sudo npm i webpack webpack-cli webpack-dev-server html-webpack-plugin babel-loader @babel/core @babel/preset-env @babel/preset-react @babel/cli css-loader style-loader -D
+
+# Tailwind
+sudo npm i tailwindcss autoprefixer postcss-cli mini-css-extract-plugin postcss-loader --save-dev
+./node_modules/.bin/tailwind init tailwind.config.js
