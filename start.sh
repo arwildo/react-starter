0ea3b639b3c3ab8cac6e87cd549fc4080ae1963f
arwildo <arwildo@gmail.com>
2020-12-24 22:11:46 +0900

    add action log to terminal

--- start.sh
+++ start.sh
@@ -1,14 +1,17 @@
 npm init
 
 # React, Webpack, Babel, CSS Loader
+echo "Installing React, Webpack, Babel, Style Loader"
 sudo npm i react react-dom -S
 sudo npm i webpack webpack-cli webpack-dev-server html-webpack-plugin babel-loader @babel/core @babel/preset-env @babel/preset-react @babel/cli css-loader style-loader -D
 
 # Tailwind
+echo "Installing Tailwind"
 sudo npm i tailwindcss autoprefixer postcss-cli mini-css-extract-plugin postcss-loader --save-dev
 ./node_modules/.bin/tailwind init tailwind.config.js
 
 # Get config files
+echo "Get config files from github repo"
 wget https://github.com/arwildo/react-starter/raw/master/config/.babelrc
 wget https://github.com/arwildo/react-starter/raw/master/config/.editorconfig
 wget https://github.com/arwildo/react-starter/raw/master/config/.gitignore
@@ -16,6 +19,7 @@ wget https://github.com/arwildo/react-starter/raw/master/config/postcss.config.j
 wget https://github.com/arwildo/react-starter/raw/master/config/webpack.config.js
 
 # Get src
+echo "Get src files from github repo"
 mkdir src
 wget https://github.com/arwildo/react-starter/raw/master/src/index.js
 wget https://github.com/arwildo/react-starter/raw/master/src/style.css
