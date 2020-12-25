47899ec3d7915956309143a0d99f1245a7c42edc
arwildo <arwildo@gmail.com>
2020-12-25 21:55:43 +0900

    add margin to container

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './style.css';
 
 const Index = () => {
   return ( 
-  <div className="bg-white mx-auto max-w-sm shadow-lg rounded-lg overflow-hidden">
+  <div className="bg-white mx-auto max-w-sm shadow-lg m-10 rounded-lg overflow-hidden">
     <div className="sm:flex sm:items-center px-6 py-4">
       <img className="block h-16 sm:h-24 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://avatars3.githubusercontent.com/u/67109815?s=200&v=4" alt="Tailwind"/>
       <div className="text-center sm:text-left sm:flex-grow">
